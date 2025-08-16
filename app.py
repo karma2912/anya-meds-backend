4a79580da7a370c710dcd3799ac6db6a9a760968
karma2912 <rajakyash23@gmail.com>
2025-08-16 18:18:36 +0530

    commit

--- app.py
+++ app.py
@@ -312,4 +312,4 @@ def predict_skin_endpoint():
 # RUN THE APP
 # =============================================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
